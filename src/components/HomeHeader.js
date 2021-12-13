b549b22dca41a1a5f025fc717cde87fb48c8f5ca
maciekm88 <maciek.m.88@gmail.com>
2021-12-13 15:43:28 +0100

    Home header styling

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import homeHeroImage from "../assets/Home-Hero-Image.jpg";
+import decoration from "../assets/Decoration.svg";
+
 
 const HomeHeader = () => {
     return (
-        <div>
-            home header
-        </div>
+        <section className="home-header">
+            <img className="home-header-img" src={homeHeroImage} alt="hero home"/>
+            <div className="home-header-content">
+                <h1 className="home-header-title">Zacznij pomagać!<br/>Oddaj niechciane rzeczy w zaufane ręce</h1>
+                <img className="home-header-decoration" src={decoration} alt="decoration pattern"/>
+                <div className="home-header-buttons">
+                    <Link className="button" to="/login"><span>ODDAJ RZECZY</span></Link>
+                    <Link className="button" to="/login"><span>ZORGANIZUJ ZBIÓRKĘ</span></Link>
+                </div>
+            </div>
+        </section>
     );
 };
 
