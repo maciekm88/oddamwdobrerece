cc66a15cf4ebd414188ec15649f271ba6cd24e10
maciekm88 <maciek.m.88@gmail.com>
2021-12-14 22:29:47 +0100

    Added HomeThreeColumns section

--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -2,9 +2,34 @@ import React from 'react';
 
 const HomeThreeColumns = () => {
     return (
-        <div>
-            home three columns
-        </div>
+        <section className="home-three-columns">
+            <div className="three-columns">
+                <div className="column">
+                    <p className="col-number">10</p>
+                    <p className="col-title">ODDANYCH WORKÓW</p>
+                    <p className="col-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma.
+                        Aliquam erat volutpat.
+                    </p>
+                </div>
+                <div className="column">
+                    <p className="col-number">5</p>
+                    <p className="col-title">WSPARTYCH ORGANIZACJI</p>
+                    <p className="col-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma.
+                        Aliquam erat volutpat.
+                    </p>
+                </div>
+                <div className="column">
+                    <p className="col-number">7</p>
+                    <p className="col-title">ZORGANIZOWANYCH ZBIÓREK</p>
+                    <p className="col-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma.
+                        Aliquam erat volutpat.
+                    </p>
+                </div>
+            </div>
+        </section>
     );
 };
 
