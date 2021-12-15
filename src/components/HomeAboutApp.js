d4199818c033694783a5fad6bde04ca6c7267f9a
maciekm88 <maciek.m.88@gmail.com>
2021-12-15 23:57:13 +0100

    Added HomeAboutApp

--- src/components/HomeAboutApp.js
+++ src/components/HomeAboutApp.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import shirt from "../assets/Icon-1.svg";
+import bag from "../assets/Icon-2.svg";
+import magnifier from "../assets/Icon-3.svg";
+import reload from "../assets/Icon-4.svg";
 
 const HomeAboutApp = () => {
     return (
-        <div>
-            about app
-        </div>
+        <section id="home-about-app">
+            <h1 className="home-about-app-title">Wystarczą 4 proste kroki</h1>
+            <div className="about-app">
+                <div className="home-about-app-container">
+                    <div className="about-app-content">
+                        <img src={shirt} alt="shirt" />
+                        <h3>Wybierz rzeczy</h3>
+                        <span>ubrania, zabawki, sprzęt i inne</span>
+                    </div>
+                    <div className="about-app-content">
+                        <img src={bag} alt="bag" />
+                        <h3>Spakuj je</h3>
+                        <span>skorzystaj z worków na śmieci</span>
+                    </div>
+                    <div className="about-app-content">
+                        <img src={magnifier} alt="magnifier" />
+                        <h3>
+                            Zdecyduj komu <br />
+                            chcesz pomóc
+                        </h3>
+                        <span>wybierz zaufane miejsce</span>
+                    </div>
+                    <div className="about-app-content">
+                        <img src={reload} alt="reload" />
+                        <h3>Zamów kuriera</h3>
+                        <span>kurier przyjedzie w dogodnym terminie</span>
+                    </div>
+                </div>
+            </div>
+            <div className="login-button">
+                <Link className="button" to="/login">
+                    <span>ODDAJ RZECZY</span>
+                </Link>
+            </div>
+        </section>
     );
 };
 
