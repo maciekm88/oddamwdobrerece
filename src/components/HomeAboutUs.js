6e10a454b4c2c97c955ff58a389a04ce1afb606e
maciekm88 <maciek.m.88@gmail.com>
2021-12-21 18:45:13 +0100

    Added HomeAboutUs section

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import signature from "../assets/Signature.svg"
+import people from "../assets/People.jpg"
 
 const HomeAboutUs = () => {
     return (
-        <div>
-            about us
-        </div>
+        <section id="home-about-us" className="home-about-us">
+            <div className="home-about-us-content">
+                <h1>"O nas"</h1>
+                <p className="about-us-content">
+                    Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery.
+                    Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.
+                </p>
+                <img src={signature} alt="signature" />
+            </div>
+            <img className="home-about-us-image" src={people} alt="people" />
+        </section>
     );
 };
 
